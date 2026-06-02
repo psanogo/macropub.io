3eba115ad5b7316dababa595dce3a97fc5f949be
Papa M Sanogo <97117540+psanogo@users.noreply.github.com>
2026-06-02 14:29:22 -0400

    Update Next.js

--- Next.js
+++ Next.js
@@ -2,3 +2,10 @@ npx create-next-app@latest macropub
 cd macropub
 npm install gray-matter remark remark-html
 npm run dev
+import Giscus from "@giscus/react";
+
+<Giscus
+  repo="psanogo/macropub.io"
+  mapping="pathname"
+  theme="dark"
+/>
