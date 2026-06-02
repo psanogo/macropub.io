b72ff162f5c53fed256bf83ca5d22f3172704fce
Papa M Sanogo <97117540+psanogo@users.noreply.github.com>
2026-06-02 12:46:38 -0400

    Create posts.ts

---
+++ src/lib/posts.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "content/posts");
+
+export function getAllPosts() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data } = matter(fileContents);
+
+    return {
+      slug,
+      ...data,
+    };
+  });
+}
